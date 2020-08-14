7a99ca4d15456e1d42b3d35c6f39f82409f880e6
ZergOfSwarm <d358468115905@gmail.com>
2020-08-14 09:34:53 +0300

    Уведомление в трее + пишет в лог + flags!

--- notify_somebar.sh
+++ notify_somebar.sh
@@ -1,48 +1,80 @@
 #!/bin/bash
-IP_LEIKKIKUJA='10.19.0.18'
+IP_LEIKKIKUJA='10.19.0.22'
 port=9753 # Указываем порт. Такой же порт должен быть и в скрипте Run_somebar.sh!
+flag='false' # Флаг опущен.
+LOCATION="/home/denis/Desktop/" # Указываем путь для удаления лог файла.
+REQUIRED_FILES="test.log" # Указываем как называется файл который нужно удалить.
+minuts=300 # Указываем количество минут по истеченни котрых удаляем лог файл. (1440 - сутки)
+LOGFILE="/home/denis/Desktop/test.log"
+#time_now=$(date +"%H:%M:%S %d-%m-%Y")
 
 green() { #https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string
-if [ -z "$(ss -tulpn | grep ':$port' 2> /dev/null)" ]; then 
-    #echo "Порт свободен!"
-    #somebar -p $port 2> /dev/null  #Где "2> /dev/null" - игнорим предупреждения! (Так не работает! Нужно запускать отдельным скриптом!)
-    /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
-    sleep 1 
-    echo -n "green" | nc -u  localhost $port  &
-else
-    #echo "Порт задействован!"
-    echo -n "green" | nc -u  localhost $port  &
-fi
+    time_now=$(date +"%H:%M:%S %d-%m-%Y")
+    #echo "Флаг сейчас - "  $flag # Для отладки
+    if [ -z "$(ss -tulpn | grep $port)" ] && [ flag != 'green' ]; then # объяснение, что такое "-z" https://linux.die.net/man/1/test 
+        #echo "Порт свободен!"
+        #somebar -p $port 2> /dev/null  #Где "2> /dev/null" - игнорим предупреждения! (Так не работает! Нужно запускать отдельным скриптом!)
+        /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
+        sleep 3
+        echo -n "green" | nc -u  localhost $port  &
+        flag=green # Поднимаем зеленый флаг "светофор"!
+        echo "$time_now Время-1 включения!" >> $LOGFILE # Пишем в лог файл.
+    else
+       if [[ $flag != 'green' ]]; then
+        #echo "Порт задействован!"
+        echo -n "green" | nc -u  localhost $port  &
+        flag=green # Поднимаем зеленый флаг "светофор"!
+        echo "$time_now Время-2 включения!" >> $LOGFILE # Пишем в лог файл.
+       fi
+    fi
 }
 
-white() {
-if [ -z "$(ss -tulpn | grep ':$port' 2> /dev/null)" ]; then
-    #echo "Порт свободен!"
-    #somebar -p $port 2> /dev/null  #Где "2> /dev/null" - игнорим предупреждения! (Так не работает! Нужно запускать отдельным скриптом!)
-    /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
-    sleep 1
-    echo -n "white" | nc -u  localhost $port  &
-else
-    #echo "Порт задействован!"
-    echo -n "white" | nc -u  localhost $port  &
-fi
+white() { #https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string
+    time_now=$(date +"%H:%M:%S %d-%m-%Y")
+    #echo "Флаг сейчас - "  $flag # Для отладки
+    if [ -z "$(ss -tulpn | grep $port)" ] && [ flag != 'white' ]; then # объяснение, что такое "-z" https://linux.die.net/man/1/test 
+        #echo "Порт свободен!"
+        #somebar -p $port 2> /dev/null  #Где "2> /dev/null" - игнорим предупреждения! (Так не работает! Нужно запускать отдельным скриптом!)
+        /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
+        sleep 3
+        echo -n "white" | nc -u  localhost $port  &
+        flag=white # Поднимаем белый флаг "светофор"!
+        echo "$time_now Время-3 ОТКлючения!" >> $LOGFILE # Пишем в лог файл.
+    else
+       if [[ $flag != 'white' ]] && [[ flag != new_flag ]]; then
+        #echo "Порт задействован!"
+        echo -n "white" | nc -u  localhost $port  &
+        #flag=$new_flag
+        flag=white # Поднимаем белый флаг "светофор"!
+        echo "$time_now Время-4 ОТКлючения!" >> $LOGFILE # Пишем в лог файл.
+       fi
+    fi
 }
 
-question() {
-if [ -z "$(ss -tulpn | grep ':$port' 2> /dev/null)" ]; then #Где "2> /dev/null" - игнорим предупреждения!
-    #echo "Порт свободен!"
-    #somebar -p $port 2> /dev/null (Так не работает! Нужно запускать отдельным скриптом!)
-    /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
-    sleep 1
-    echo -n "question" | nc -u  localhost $port  &
-else
-    #echo "Порт задействован!"
-    echo -n "question" | nc -u  localhost $port  &
-fi
+question() { #https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string
+    time_now=$(date +"%H:%M:%S %d-%m-%Y")
+    #echo "Флаг сейчас - "  $flag # Для отладки
+    if [ -z "$(ss -tulpn | grep $port)" ] && [ flag != 'question' ]; then # объяснение, что такое "-z" https://linux.die.net/man/1/test 
+        #echo "Порт свободен!"
+        #somebar -p $port 2> /dev/null  #Где "2> /dev/null" - игнорим предупреждения! (Так не работает! Нужно запускать отдельным скриптом!)
+        /home/denis/.scripts/run_somebar.sh &> /dev/null # Запуск скрипта который включает somebar на порту $port
+        sleep 3
+        echo -n "question" | nc -u  localhost $port  &
+        flag=question # Поднимаем флаг '?' "светофор"!
+        echo "$time_now Время-5 когда отвалился инет!" >> $LOGFILE # Пишем в лог файл.
+    else
+       if [[ $flag != 'question' ]] && [[ flag != new_flag ]]; then
+        #echo "Порт задействован!"
+        echo -n "question" | nc -u  localhost $port  &
+        #flag=$new_flag
+        flag=question # Поднимаем флаг '?' "светофор"!
+        echo "$time_now Время-6 когда отвалился инет!" >> $LOGFILE # Пишем в лог файл.
+       fi
+    fi
 }
 
 function ping_ip {
-    #echo "Пингую 'IP' - ${IP_LEIKKIKUJA}" 
+    #echo "Пингую 'IP' - ${IP_LEIKKIKUJA}"
     ping -q -c2 ${IP_LEIKKIKUJA} &> /dev/null # Скрываем вывод команды "ping" при помощи (&> /dev/null) 
     if [ $? -eq 0 ]
     then
@@ -54,24 +86,17 @@ function ping_ip {
     fi
 }
 
-TIMESTAMP=`date +%s`
 while [ 1 ]
   do
-    nc -z -w 5 8.8.8.8 53  >/dev/null 2>&1
-online=$?
-    TIME=`date +%s`
+    find $LOCATION -name $REQUIRED_FILES -type f -mmin +$minuts -delete # Удаляем лог файл по истечении времени!
+    nc -z -w 5 8.8.8.8 53
+    online=$?
     if [ $online -eq 0 ]; then
-      #echo "`date +%Y-%m-%d_%H:%M:%S_%Z` 1 $(($TIME-$TIMESTAMP))" | tee -a log.csv
-      #echo "`date +%Y-%m-%d_%H:%M:%S_%Z`"
       #echo "Инет есть!"
       ping_ip
     else
-      #echo "`date +%Y-%m-%d_%H:%M:%S_%Z` 0 $(($TIME-$TIMESTAMP))" | tee -a log.csv
-      #echo "`date +%Y-%m-%d_%H:%M:%S_%Z`"
       #echo "Нет интернет соединения!"
       question
     fi
-    TIMESTAMP=$TIME
-    sleep 5
+    sleep 1
   done;
-
